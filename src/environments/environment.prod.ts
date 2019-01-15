d004c22a413cba33566eb2d60f071efbed0568db
Amit Gaikwad <amitgaikwad85@gmail.com>
2019-01-15 19:28:08 +0530

    changed to app.listen

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  app_url: 'https://geotag-leaflet.herokuapp.com',
+  app_url: 'https://plotter-map.herokuapp.com',
   // app_url: 'http://localhost:5000',
   mapbox: {
     accessToken: 'pk.eyJ1IjoiYW1pdGdhaWt3YWQ4NSIsImEiOiJjanFzMDYwbHEwaHd4NDJsanIzZ2hqbDFyIn0.H4c917HglXZhvgSdrTjhZA'
